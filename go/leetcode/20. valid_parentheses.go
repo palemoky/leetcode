b4f7184344a3bf2bf9c2b12d8a3358448200dd70
Palemoky <palemoky@gmail.com>
2025-06-01 11:04:03 +0800

    refactor: rename isValid function and add isValidSwitchCase implementation

--- go/leetcode/20. valid_parentheses.go
+++ go/leetcode/20. valid_parentheses.go
@@ -1,23 +1,48 @@
 package leetcode
 
 // Time: O(n), Space: O(n)
-func isValid(s string) bool {
-	braces := []rune{}
+func isValidIfElse(s string) bool {
+	stack := []rune{}
 	pairs := map[rune]rune{
 		')': '(',
 		']': '[',
 		'}': '{',
 	}
 
-	for _, v := range s {
-		if v == '(' || v == '[' || v == '{' {
-			braces = append(braces, v)
+	for _, char := range s {
+		if char == '(' || char == '[' || char == '{' {
+			stack = append(stack, char)
 		} else {
-			if len(braces) == 0 || braces[len(braces)-1] != pairs[v] {
+			if len(stack) == 0 || stack[len(stack)-1] != pairs[char] {
 				return false
 			}
-			braces = braces[:len(braces)-1]
+			stack = stack[:len(stack)-1]
 		}
 	}
-	return len(braces) == 0
+	return len(stack) == 0
+}
+
+// Time: O(n), Space: O(n)
+func isValidSwitchCase(s string) bool {
+	stack := []rune{}
+	pairs := map[rune]rune{
+		')':'(',
+		']':'[',
+		'}':'{',
+	}
+
+	for _, char := range s {
+		switch char {
+			case '(', '[', '{':
+				stack = append(stack, char)
+			case ')', ']', '}':
+				if len(stack) > 0 && stack[len(stack)-1] == pairs[char] {
+					stack = stack[:len(stack)-1]
+				} else {
+					return false
+				}
+		}
+	}
+
+	return len(stack) == 0
 }
