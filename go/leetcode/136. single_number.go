6c7b4a729747a11b1806d5f5f39a39517cbc0c1b
Palemoky <palemoky@gmail.com>
2025-06-01 18:45:00 +0800

    feat: solved promble #136

---
+++ go/leetcode/136. single_number.go
@@ -0,0 +1,16 @@
+package leetcode
+
+func singleNumberHashMap(nums []int) int {
+	counter := map[int]int{}
+	for _, num := range nums {
+		counter[num]++
+	}
+
+	for num, count := range counter {
+		if count == 1 {
+			return num
+		}
+	}
+
+	return 0
+}
