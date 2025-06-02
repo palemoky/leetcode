0357dc035ffa82bd391c42a8a51989388fdd51fe
Palemoky <palemoky@gmail.com>
2025-06-02 20:03:48 +0800

    feat: solved problem #136

---
+++ go/136.singleNumber/136. single_number.go
@@ -0,0 +1,27 @@
+package leetcode
+
+// Time: O(n), Space: O(n)
+func singleNumberHashMap(nums []int) int {
+	counter := map[int]int{}
+	for _, num := range nums {
+		counter[num]++
+	}
+
+	for num, count := range counter {
+		if count == 1 {
+			return num
+		}
+	}
+
+	return 0
+}
+
+// Time: O(n), Space: O(1)
+func singleNumberBitWise(nums []int) int {
+	ans := 0
+	for _, num := range nums {
+		ans ^= num
+	}
+
+	return ans
+}
---
+++ go/136.singleNumber/136. single_number_test.go
@@ -0,0 +1,58 @@
+package leetcode
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSingleNumberHashMap(t *testing.T) {
+	testCases := []struct {
+		name string
+		nums []int
+		want int
+	}{
+		{"Example 1", []int{2, 2, 1}, 1},
+		{"Example 2", []int{4, 1, 2, 1, 2}, 4},
+		{"Example 3", []int{1}, 1},
+		{"Example 4", []int{}, 0},
+	}
+
+	algorithms := []struct {
+		name string
+		fn   func([]int) int
+	}{
+		{"HashMap", singleNumberHashMap},
+		{"BitWise", singleNumberBitWise},
+	}
+
+	for _, algo := range algorithms {
+		t.Run(algo.name, func(t *testing.T) {
+			for _, tc := range testCases {
+				t.Run(tc.name, func(t *testing.T) {
+					got := algo.fn(tc.nums)
+					assert.Equal(t, tc.want, got, "%s: input=%v", algo.name, tc.nums)
+				})
+			}
+		})
+	}
+}
+
+func BenchmarkSingleNumber(b *testing.B) {
+    nums := []int{1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5}
+    algorithms := []struct {
+        name string
+        fn   func([]int) int
+    }{
+        {"HashMap", singleNumberHashMap},
+        {"BitWise", singleNumberBitWise},
+    }
+
+    for _, algo := range algorithms {
+        b.Run(algo.name, func(b *testing.B) {
+            for b.Loop() {
+                algo.fn(nums)
+            }
+        })
+    }
+}
