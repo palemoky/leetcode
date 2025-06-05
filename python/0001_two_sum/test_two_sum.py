85e65903d2eb6bcc17a11dffa4b41804df87dfc6
Palemoky <palemoky@gmail.com>
2025-06-05 18:45:23 +0800

    feat: solved problem #1

---
+++ python/0001_two_sum/test_two_sum.py
@@ -0,0 +1,23 @@
+import unittest
+from two_sum import Solution
+
+class TestTwoSum(unittest.TestCase):
+    def setUp(self):
+        self.s = Solution()
+
+    def test_two_sum_brute_force(self):
+        self.assertEqual(self.s.twoSumBruteForce([2, 7, 11, 15], 9), [0, 1])
+        self.assertEqual(self.s.twoSumBruteForce([3, 2, 4], 6), [1, 2])
+        self.assertEqual(self.s.twoSumBruteForce([3, 3], 6), [0, 1])
+        self.assertEqual(self.s.twoSumBruteForce([1, 2], 3), [0, 1])
+        self.assertEqual(self.s.twoSumBruteForce([0, 4, 3, 0], 0), [0, 3])
+
+    def test_two_sum_hash_map(self):
+        self.assertEqual(self.s.twoSumHashMap([2, 7, 11, 15], 9), [0, 1])
+        self.assertEqual(self.s.twoSumHashMap([3, 2, 4], 6), [1, 2])
+        self.assertEqual(self.s.twoSumHashMap([3, 3], 6), [0, 1])
+        self.assertEqual(self.s.twoSumHashMap([1, 2], 3), [0, 1])
+        self.assertEqual(self.s.twoSumHashMap([0, 4, 3, 0], 0), [0, 3])
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ python/0001_two_sum/two_sum.py
@@ -0,0 +1,17 @@
+class Solution:
+    def twoSumBruteForce(self, nums: List[int], target: int) -> List[int]:
+        n = len(nums)
+        for i in range(n):
+            for j in range(i + 1, n):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+        
+        return []
+
+    def twoSumHashMap(self, nums: List[int], target: int) -> List[int]:
+        hashtable = dict()
+        for i, num in enumerate(nums):
+            if target - num in hashtable:
+                return [hashtable[target - num], i]
+            hashtable[nums[i]] = i
+        return []
