9f096fee3c4d48caf3863cffc7fa7149d8bcd937
Palemoky <palemoky@gmail.com>
2025-07-25 07:28:50 +0800

    feat: implement removeNthFromEndTwoPointers function and add corresponding unit tests

--- go/core/practice/linked_list.go
+++ go/core/practice/linked_list.go
@@ -15,10 +15,10 @@ func reverse(head *ListNode) *ListNode {
 	var prev, next *ListNode // 注意不能写 &SinglyNode{}，这样的初始值是0和nil，只有*SinglyNode的值才是nil
 	current := head
 	for current != nil {
-		next = current.Next // 1. save next node
-		current.Next = prev // 2. reverse
-		prev = current      // 3. move prev
-		current = next      // 4. move current
+		next = current.Next // 1. 反转节点前先保存指针信息，避免丢失
+		current.Next = prev // 2. 反转节点
+		prev = current      // 3. 移动 prev
+		current = next      // 4. 移动 current
 	}
 
 	return prev
@@ -154,3 +154,27 @@ func findCycleEnteryMathTwoPointers(head *ListNode) *ListNode {
 }
 
 // 删除链表的倒数第N个节点
+func removeNthFromEndTwoPointers(head *ListNode, n int) *ListNode {
+	// 1. 可能删除任意位置的节点，因此需将虚拟节点指向头结点
+	dummy := &ListNode{Next: head}
+	fast, slow := dummy, dummy
+
+	// 2. 让快指针移动n位
+	// 边界条件：~~空链表、n > len(list)、~~一个节点的链表、删除头结点、删除尾节点
+	// 题目已经限制节点数量为[1, 30]，n的范围[1, len(list)]
+	for range n {
+		fast = fast.Next
+	}
+
+	// 3. 同时移动快慢指针，直至快指针到达节点尾部
+	for fast.Next != nil {
+		fast, slow = fast.Next, slow.Next
+	}
+
+	// 4. 删除slow所在的节点
+	// A->B->C->D to A->B->D
+	slow.Next = slow.Next.Next
+
+	// 返回头结点
+	return dummy.Next
+}
