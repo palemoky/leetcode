e6e2a383ab19ad70fa6ceb93ab0828d9cc0e83f7
Palemoky <palemoky@gmail.com>
2025-07-28 16:04:04 +0800

    feat: solved problem #206

---
+++ go/solutions/0206_reverse_linked_list/reverse_linked_list.go
@@ -0,0 +1,21 @@
+package reverse_linked_list
+
+import "leetcode/go/solutions/utils"
+
+// Time: O(n), Space: O(1)
+func reverseList(head *utils.ListNode) *utils.ListNode {
+	if head == nil {
+		return nil
+	}
+
+	var prev, next *utils.ListNode // 注意不能写 &SinglyNode{}，这样的初始值是0和nil，只有*SinglyNode的值才是nil
+	current := head
+	for current != nil {
+		next = current.Next // 1. 反转节点前先保存指针信息，避免丢失
+		current.Next = prev // 2. 反转节点
+		prev = current      // 3. 移动 prev
+		current = next      // 4. 移动 current
+	}
+
+	return prev
+}
---
+++ go/solutions/0206_reverse_linked_list/reverse_linked_list_test.go
@@ -0,0 +1,51 @@
+package reverse_linked_list
+
+import (
+	"leetcode/go/solutions/utils"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestReverse(t *testing.T) {
+	type testCase struct {
+		name     string
+		input    *utils.ListNode
+		expected *utils.ListNode
+	}
+
+	testCases := []testCase{
+		{
+			name:     "nil list",
+			input:    nil,
+			expected: nil,
+		},
+		{
+			name:     "single node list",
+			input:    utils.NewList([]int{1}),
+			expected: utils.NewList([]int{1}),
+		},
+		{
+			name:     "two nodes list",
+			input:    utils.NewList([]int{1, 2}),
+			expected: utils.NewList([]int{2, 1}),
+		},
+		{
+			name:     "multiple nodes list (odd)",
+			input:    utils.NewList([]int{1, 2, 3, 4, 5}),
+			expected: utils.NewList([]int{5, 4, 3, 2, 1}),
+		},
+		{
+			name:     "multiple nodes list (even)",
+			input:    utils.NewList([]int{1, 2, 3, 4}),
+			expected: utils.NewList([]int{4, 3, 2, 1}),
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			actual := reverseList(tc.input)
+			assert.Equal(t, utils.ToSlice(tc.expected), utils.ToSlice(actual))
+		})
+	}
+}
