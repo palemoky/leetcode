d7e4026c4c80b779a12bebbbfb4ed5c5e2b04ebe
Palemoky <palemoky@gmail.com>
2025-08-12 18:38:31 +0800

    fix: correct variable declaration for next in reverseList function

--- go/solutions/0206_reverse_linked_list/reverse_linked_list.go
+++ go/solutions/0206_reverse_linked_list/reverse_linked_list.go
@@ -8,13 +8,13 @@ func reverseList(head *utils.ListNode) *utils.ListNode {
 		return nil
 	}
 
-	var prev, next *utils.ListNode // 注意不能写 &SinglyNode{}，这样的初始值是0和nil，只有*SinglyNode的值才是nil
+	var prev *utils.ListNode // 注意不能写 &SinglyNode{}，这样的初始值是0和nil，只有*SinglyNode的值才是nil
 	current := head
 	for current != nil {
-		next = current.Next // 1. 反转节点前先保存指针信息，避免丢失
-		current.Next = prev // 2. 反转节点
-		prev = current      // 3. 移动 prev
-		current = next      // 4. 移动 current
+		next := current.Next // 1. 反转节点前先保存指针信息，避免丢失
+		current.Next = prev  // 2. 反转节点
+		prev = current       // 3. 移动 prev
+		current = next       // 4. 移动 current
 	}
 
 	return prev
