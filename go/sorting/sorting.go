96aead7c7a46255c121307059c4633b9e01b3ce6
Palemoky <palemoky@gmail.com>
2025-08-18 18:03:55 +0800

    docs: improve sorting algorithms documentation with detailed comments and structure

--- go/sorting/sorting.go
+++ go/sorting/sorting.go
@@ -5,6 +5,8 @@ import (
 	"math"
 )
 
+// 所有排序算法均为升序实现
+
 // 冒泡排序：不断两两比较交换，直到所有元素有序
 // 优化点：已排序区可跳过；全部有序可提前结束遍历
 // Time: O(n^2)
@@ -66,7 +68,8 @@ func selection(nums []int) []int {
 	return nums
 }
 
-// 插入排序：
+// 插入排序：从第二个元素开始，不断将之后的元素放在之前的已排序位置
+// Time: O(n^2)
 func insertion(nums []int) []int {
 	n := len(nums)
 	if n < 2 {
@@ -77,13 +80,17 @@ func insertion(nums []int) []int {
 		return nums
 	}
 
+	// [0,i)为已排序区，[i,n)为待排序区
 	for i := 1; i < n; i++ {
-		key := nums[i]
+		key := nums[i] // key 为从桌上拿起的新牌
 		j := i - 1
+		// j 负责逆序遍历已排序区，为 key 找到插入位置
 		for j >= 0 && nums[j] > key {
+			// 向右移动元素，腾出插入空间
 			nums[j+1] = nums[j]
 			j--
 		}
+		// 将 key 插入正确位置
 		nums[j+1] = key
 	}
 
@@ -101,90 +108,112 @@ func quick(nums []int) []int {
 		return nums
 	}
 
-	var qsort func([]int, int, int)
-	qsort = func(arr []int, left, right int) {
-		if left >= right {
-			return
-		}
-		pivot := arr[left]
-		l, r := left, right
-		for l < r {
-			for l < r && arr[r] >= pivot {
-				r--
-			}
-			arr[l] = arr[r]
-			for l < r && arr[l] <= pivot {
-				l++
-			}
-			arr[r] = arr[l]
-		}
-		arr[l] = pivot
-		qsort(arr, left, l-1)
-		qsort(arr, l+1, right)
-	}
-
-	qsort(nums, 0, n-1)
+	// 调用私有的递归辅助函数
+	quickSortRecursive(nums, 0, n-1)
 
 	return nums
 }
 
+// quickSortRecursive 是递归的核心
+func quickSortRecursive(nums []int, left, right int) {
+	if left >= right {
+		return
+	}
+
+	// 分区，并拿到 pivot 的最终位置
+	pivotIndex := quickSortPartition(nums, left, right)
+
+	// 递归地对左右两部分进行排序
+	quickSortRecursive(nums, left, pivotIndex-1)
+	quickSortRecursive(nums, pivotIndex+1, right)
+}
+
+// quickSortPartition 负责分区操作，并返回 pivot 的索引
+func quickSortPartition(nums []int, left, right int) int {
+	pivotValue := nums[left] // 选择第一个元素作为 pivot
+	l, r := left, right
+
+	for l < r {
+		// 从右向左找第一个小于 pivot 的数
+		for l < r && nums[r] >= pivotValue {
+			r--
+		}
+
+		nums[l] = nums[r]
+		// 从左向右找第一个大于 pivot 的数
+		for l < r && nums[l] <= pivotValue {
+			l++
+		}
+
+		nums[r] = nums[l]
+	}
+
+	// 将 pivot 放回正确的位置
+	nums[l] = pivotValue
+
+	return l
+}
+
 // 归并排序
 func merge(nums []int) []int {
-	n := len(nums)
-	if n < 2 {
-		if n == 0 {
+	if len(nums) < 2 {
+		if nums == nil {
 			return []int{}
 		}
 
 		return nums
 	}
 
-	var msort func([]int, int, int)
-	var mergeArr func([]int, int, int, int)
-	msort = func(arr []int, left, right int) {
-		if left >= right {
-			return
-		}
-		mid := left + (right-left)/2
-		msort(arr, left, mid)
-		msort(arr, mid+1, right)
-		mergeArr(arr, left, mid, right)
-	}
-
-	mergeArr = func(arr []int, left, mid, right int) {
-		tmp := make([]int, right-left+1)
-		i, j, k := left, mid+1, 0
-		for i <= mid && j <= right {
-			if arr[i] <= arr[j] {
-				tmp[k] = arr[i]
-				i++
-			} else {
-				tmp[k] = arr[j]
-				j++
-			}
-			k++
-		}
-
-		for i <= mid {
-			tmp[k] = arr[i]
-			i++
-			k++
-		}
-
-		for j <= right {
-			tmp[k] = arr[j]
-			j++
-			k++
-		}
-
-		for p := 0; p < len(tmp); p++ {
-			arr[left+p] = tmp[p]
-		}
-	}
-
-	msort(nums, 0, n-1)
+	mergeSortRecursive(nums, 0, len(nums)-1)
 
 	return nums
+}
+
+// mergeSortRecursive 是递归的核心
+func mergeSortRecursive(nums []int, left, right int) {
+	if left >= right {
+		return
+	}
+
+	mid := left + (right-left)/2
+	mergeSortRecursive(nums, left, mid)
+	mergeSortRecursive(nums, mid+1, right)
+	mergeHalves(nums, left, mid, right)
+}
+
+// mergeHalves 负责合并两个已排序的子数组
+func mergeHalves(nums []int, left, mid, right int) {
+	// 创建一个临时切片来存储合并后的结果
+	tmp := make([]int, right-left+1)
+	i, j, k := left, mid+1, 0
+
+	// 比较左右两部分，将较小的元素放入 tmp
+	for i <= mid && j <= right {
+		if nums[i] <= nums[j] {
+			tmp[k] = nums[i]
+			i++
+		} else {
+			tmp[k] = nums[j]
+			j++
+		}
+		k++
+	}
+
+	// 将剩余的元素（如果 L 或 R 部分有剩下）拷贝到 tmp
+	for i <= mid {
+		tmp[k] = nums[i]
+		i++
+		k++
+	}
+
+	for j <= right {
+		tmp[k] = nums[j]
+		j++
+		k++
+	}
+
+	// 将排好序的 tmp 内容拷贝回原始的 nums 切片的对应位置
+	copy(nums[left:right+1], tmp)
 }
 
 // 希尔排序
