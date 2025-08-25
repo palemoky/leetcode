08e1dab13257d560a2dc36954814389f501917af
Palemoky <palemoky@gmail.com>
2025-08-25 18:02:16 +0800

    feat: solved problem #102

---
+++ go/solutions/0102_binary_tree_level_order_traversal/level_order.go
@@ -0,0 +1,39 @@
+package binary_tree_level_order_traversal
+
+import "leetcode/go/solutions/utils"
+
+// Time: O(n), Space: O(n)
+func levelOrder(root *utils.TreeNode) [][]int {
+	nums := [][]int{}
+	if root == nil {
+		return nums
+	}
+
+	queue := []*utils.TreeNode{root} // 将整棵树放入队列
+	// 遍历树的深度
+	for len(queue) > 0 { // 由于最后不断地弹出已遍历元素，此处必须是 len(queue)
+		levelSize := len(queue)
+		row := []int{} // 收集每层的结果
+		// 当前行元素的最大长度就等于树的深度，依次读取当前深度的节点
+		for i := range levelSize {
+			// 左右节点可能为空，因此非空时放入下一层
+			if queue[i].Left != nil {
+				queue = append(queue, queue[i].Left) // enqueue
+			}
+
+			if queue[i].Right != nil {
+				queue = append(queue, queue[i].Right) //enqueue
+			}
+
+			// 读取当前层的值
+			row = append(row, queue[i].Val)
+		}
+
+		// 裁切掉已扫描过的节点
+		queue = queue[levelSize:] // dequeue
+		// 将当前深度的节点放入结果集中
+		nums = append(nums, row)
+	}
+
+	return nums
+}
---
+++ go/solutions/0102_binary_tree_level_order_traversal/level_order_test.go
@@ -0,0 +1,31 @@
+package binary_tree_level_order_traversal
+
+import (
+	"leetcode/go/solutions/utils"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLevelOrder(t *testing.T) {
+	t.Parallel()
+	testCases := []struct {
+		name     string
+		vals     []any
+		expected [][]int
+	}{
+		{"Empty tree", []any{}, [][]int{}},
+		{"Single node", []any{1}, [][]int{{1}}},
+		{"Full tree", []any{1, 2, 3, 4, 5, 6, 7}, [][]int{{1}, {2, 3}, {4, 5, 6, 7}}},
+		{"Tree with nils", []any{1, 2, 3, nil, 4}, [][]int{{1}, {2, 3}, {4}}},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			root := utils.BuildTree(tc.vals)
+			got := levelOrder(root)
+			assert.Equal(t, tc.expected, got)
+		})
+	}
+}
