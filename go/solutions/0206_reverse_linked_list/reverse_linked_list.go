8e09e56d8823bcef02b3b3ed0448e5bde094086a
Palemoky <palemoky@gmail.com>
2025-09-04 18:13:07 +0800

    refactor: improve comment formatting for variable initialization in reverseList function

--- go/solutions/0206_reverse_linked_list/reverse_linked_list.go
+++ go/solutions/0206_reverse_linked_list/reverse_linked_list.go
@@ -8,7 +8,7 @@ func reverseList(head *utils.ListNode) *utils.ListNode {
 		return nil
 	}
 
-	var prev *utils.ListNode // 注意不能写 &SinglyNode{}，这样的初始值是0和nil，只有*SinglyNode的值才是nil
+	var prev *utils.ListNode // 注意不能写 &SinglyNode{}，这样的初始值是 0 和 nil，只有 *SinglyNode 的值才是 nil
 	current := head
 	for current != nil {
 		next := current.Next // 1. 反转节点前先保存指针信息，避免丢失
