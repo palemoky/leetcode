7356c9b2157573a3217977ce9fd1ded9ca401399
Palemoky <palemoky@gmail.com>
2025-09-06 20:50:21 +0800

    feat: solved problem #739

---
+++ go/solutions/0739_daily_temperatures/daily_temperatures.go
@@ -0,0 +1,38 @@
+package daily_temperatures
+
+// 解法一：暴力解法
+// Time: O(n^2), Space: O(n)
+func dailyTemperaturesBruteForce(temperatures []int) []int {
+	n := len(temperatures)
+	ans := make([]int, n)
+	for i := range n {
+		for j := i + 1; j < n; j++ {
+			if temperatures[j] > temperatures[i] {
+				ans[i] = j - i
+				break
+			}
+		}
+	}
+
+	return ans
+}
+
+// 解法二（推荐）：单调递减栈，通过不断维护一个栈顶小于当前元素的栈来求解。求解时最好画个栈的过程
+// Time: O(n), Space: O(n)
+func dailyTemperaturesStack(temperatures []int) []int {
+	tLen := len(temperatures)
+	ans := make([]int, tLen)
+
+	stack := []int{} // 栈内存放 temperatures 的索引
+	for i := range tLen {
+		// 如果当前元素 > 栈顶元素，则不断弹出栈中的元素，直至当前元素 < 栈顶元素
+		for len(stack) > 0 && temperatures[i] > temperatures[stack[len(stack)-1]] {
+			top := stack[len(stack)-1]   // 取栈顶值
+			ans[top] = i - top           // 将差值更新到对应的位置
+			stack = stack[:len(stack)-1] // pop
+		}
+		stack = append(stack, i) // push
+	}
+
+	return ans
+}
---
+++ go/solutions/0739_daily_temperatures/daily_temperatures_test.go
@@ -0,0 +1,42 @@
+package daily_temperatures
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDailyTemperatures(t *testing.T) {
+	t.Parallel()
+	cases := []struct {
+		name  string
+		temps []int
+		want  []int
+	}{
+		{"mixed_fluctuation", []int{73, 74, 75, 71, 69, 72, 76, 73}, []int{1, 1, 4, 2, 1, 1, 0, 0}},
+		{"strictly_increasing_short", []int{30, 40, 50, 60}, []int{1, 1, 1, 0}},
+		{"sparse_peaks", []int{30, 60, 90}, []int{1, 1, 0}},
+		{"all_same", []int{50, 50, 50, 50}, []int{0, 0, 0, 0}},
+		{"strictly_decreasing", []int{100, 90, 80, 70}, []int{0, 0, 0, 0}},
+		{"strictly_increasing", []int{10, 20, 30, 40}, []int{1, 1, 1, 0}},
+		{"single", []int{42}, []int{0}},
+		{"empty", []int{}, []int{}},
+	}
+
+	funcsToTest := map[string]func(temperatures []int) []int{
+		"BruteForce": dailyTemperaturesBruteForce,
+		"Stack":      dailyTemperaturesStack,
+	}
+
+	for fnName, fn := range funcsToTest {
+		t.Run(fnName, func(t *testing.T) {
+			for _, tc := range cases {
+				t.Run(tc.name, func(t *testing.T) {
+					t.Parallel()
+					got := fn(append([]int{}, tc.temps...))
+					assert.Equal(t, tc.want, got)
+				})
+			}
+		})
+	}
+}
