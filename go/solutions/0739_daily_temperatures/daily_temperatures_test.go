593b5f338195e6465a04303621f4aef251eacd1b
Palemoky <palemoky@gmail.com>
2025-09-26 21:30:06 +0800

    refactor: improve test case structure and simplify temperature function calls in daily temperatures tests

--- go/solutions/0739_daily_temperatures/daily_temperatures_test.go
+++ go/solutions/0739_daily_temperatures/daily_temperatures_test.go
@@ -13,20 +13,52 @@ func TestDailyTemperatures(t *testing.T) {
 		temps []int
 		want  []int
 	}{
-		{"mixed_fluctuation", []int{73, 74, 75, 71, 69, 72, 76, 73}, []int{1, 1, 4, 2, 1, 1, 0, 0}},
-		{"strictly_increasing_short", []int{30, 40, 50, 60}, []int{1, 1, 1, 0}},
-		{"sparse_peaks", []int{30, 60, 90}, []int{1, 1, 0}},
-		{"all_same", []int{50, 50, 50, 50}, []int{0, 0, 0, 0}},
-		{"strictly_decreasing", []int{100, 90, 80, 70}, []int{0, 0, 0, 0}},
-		{"strictly_increasing", []int{10, 20, 30, 40}, []int{1, 1, 1, 0}},
-		{"single", []int{42}, []int{0}},
-		{"empty", []int{}, []int{}},
+		{
+			name:  "mixed_fluctuation",
+			temps: []int{73, 74, 75, 71, 69, 72, 76, 73},
+			want:  []int{1, 1, 4, 2, 1, 1, 0, 0},
+		},
+		{
+			name:  "strictly_increasing_short",
+			temps: []int{30, 40, 50, 60},
+			want:  []int{1, 1, 1, 0},
+		},
+		{
+			name:  "sparse_peaks",
+			temps: []int{30, 60, 90},
+			want:  []int{1, 1, 0},
+		},
+		{
+			name:  "all_same",
+			temps: []int{50, 50, 50, 50},
+			want:  []int{0, 0, 0, 0},
+		},
+		{
+			name:  "strictly_decreasing",
+			temps: []int{100, 90, 80, 70},
+			want:  []int{0, 0, 0, 0},
+		},
+		{
+			name:  "strictly_increasing",
+			temps: []int{10, 20, 30, 40},
+			want:  []int{1, 1, 1, 0},
+		},
+		{
+			name:  "single",
+			temps: []int{42},
+			want:  []int{0},
+		},
+		{
+			name:  "empty",
+			temps: []int{},
+			want:  []int{},
+		},
 	}
 
 	funcsToTest := map[string]func(temperatures []int) []int{
-		"BruteForce":       dailyTemperaturesBruteForce,
-		"StackLeftToRight": dailyTemperaturesStackLeftToRight,
-		"StackRightToLeft": dailyTemperaturesStackRightToLeft,
+		"BruteForce": dailyTemperaturesBruteForce,
+		"StackLTR":   dailyTemperaturesStackLeftToRight,
+		"StackRTL":   dailyTemperaturesStackRightToLeft,
 	}
 
 	for fnName, fn := range funcsToTest {
@@ -34,7 +66,7 @@ func TestDailyTemperatures(t *testing.T) {
 			for _, tc := range cases {
 				t.Run(tc.name, func(t *testing.T) {
 					t.Parallel()
-					got := fn(append([]int{}, tc.temps...))
+					got := fn(tc.temps)
 					assert.Equal(t, tc.want, got)
 				})
 			}
