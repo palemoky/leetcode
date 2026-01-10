d1d8f9f5848902702b7f31f4d5578f5b73f2eee0
Palemoky <palemoky@gmail.com>
2026-01-10 16:56:55 +0800

    docs: update #27

--- go/solutions/0027_remove_element/remove_element.go
+++ go/solutions/0027_remove_element/remove_element.go
@@ -16,14 +16,46 @@ func removeElementTwoPointers(nums []int, val int) int {
 
 // 解法二：双指针 + 交换
 // 把所有 val 元素移到数组末尾：左指针遍历数组，遇到 val 元素时，移动到数组末尾
+
+// nums = [2, 3, 2], val = 2
+// Init: left=0, right=2
+//
+//	[2, 3, 2]
+//	 ↑     ↑
+//
+// Step 1: nums[0]==2，swap
+//
+//	[2, 3, 2] → nums[0]=nums[2]
+//	 ↑     ↑
+//	right--
+//	[2, 3, 2]
+//	 ↑  ↑
+//
+// Step 2: nums[0]==2，swap
+//
+//	[3, 3, 2] → nums[0]=nums[1]
+//	 ↑  ↑
+//	right--
+//	[3, 3, 2]
+//	 ↑↑
+//
+// Step 3: nums[0]==3，不是val
+//
+//	left++
+//	[3, 3, 2]
+//	  ↑
+//	left > right，done
+//
+
 // Time: O(n), Space: O(1)
 func removeElementSwap(nums []int, val int) int {
 	left, right := 0, len(nums)-1
 
 	for left <= right {
 		if nums[left] == val {
-			// 将右边的元素移到左边
+			// 因为题目中并不要求 k 个元素之外的元素，因此只需将右边的元素移到左边即可
 			nums[left] = nums[right]
+			// 注意此时不能 left++，因为右边交换过来的元素也可能是 val
 			right--
 		} else {
 			left++
