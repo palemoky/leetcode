4aab9e711592926af002af8c0dc1fe23c6e28f2a
Palemoky <palemoky@gmail.com>
2026-01-29 11:08:37 +0800

    refactor: #206

--- go/solutions/0206_reverse_linked_list/reverse_linked_list.go
+++ go/solutions/0206_reverse_linked_list/reverse_linked_list.go
@@ -4,19 +4,19 @@ import "leetcode/go/solutions/utils"
 
 // Time: O(n), Space: O(1)
 func reverseList(head *utils.ListNode) *utils.ListNode {
-	if head == nil {
-		return nil
+	// 注意不能写 &SinglyNode{}，这样的初始值是 0 和 nil，只有 *SinglyNode 的值才是 nil
+	var prev *utils.ListNode
+	// 反转链表类似交换变量：先暂存被修改的值，再修改指针
+	// 1→2→3 reverse 1←2→3
+	// ↑             ↑ ↑
+	// head       prev head
+	for head != nil {
+		next := head.Next // 1. 反转节点前先保存指针信息，避免丢失
+		head.Next = prev  // 2. 反转节点
+		prev = head       // 3. 移动 prev
+		head = next       // 4. 移动 head
 	}
 
-	var prev *utils.ListNode // 注意不能写 &SinglyNode{}，这样的初始值是 0 和 nil，只有 *SinglyNode 的值才是 nil
-	current := head
-	for current != nil {
-		next := current.Next // 1. 反转节点前先保存指针信息，避免丢失
-		current.Next = prev  // 2. 反转节点
-		prev = current       // 3. 移动 prev
-		current = next       // 4. 移动 current
-	}
-
-	// current 始终比 prev 快一步，所以当 current 在 nil 节点时，prev 正好处在最后一个有效节点
+	// head 始终比 prev 快一步，所以当 head 在 nil 节点时，prev 正好处在最后一个有效节点
 	return prev
 }
