1722d3f266362a227aac9681429ed73980a2511f
Palemoky <palemoky@gmail.com>
2026-05-01 11:49:20 +0800

    refactor: #3 optimize lengthOfLongestSubstringSlidingWindow function for clarity and performance

--- go/solutions/0003_longest_substring_without_repeating_characters/length_of_longest_substring.go
+++ go/solutions/0003_longest_substring_without_repeating_characters/length_of_longest_substring.go
@@ -3,21 +3,18 @@ package length_of_longest_substring
 // 解法一：哈希表 + 滑动窗口
 // Time: O(n), Space: O(n)
 func lengthOfLongestSubstringSlidingWindow(s string) int {
-	charIndex := map[byte]int{}
+	lastSeen := map[rune]int{} // 反向映射哈希表
 
 	left, maxLen := 0, 0
-	for right := range s {
-		// 出现重复字符时，将左指针移动到重复字符索引的 +1 位置
-		// 注意检查 prevIndex >= left，防止左指针回退
-		if prevIndex, exists := charIndex[s[right]]; exists && prevIndex >= left {
-			left = prevIndex + 1
+	for right, char := range s {
+		// 注意检查 prev >= left，防止左指针回退
+		if prev, seen := lastSeen[char]; seen && prev >= left {
+			left = prev + 1 // 出现重复字符时，将左指针移动到重复字符索引的 +1 位置
 		}
 
-		// 记录字符最新出现的索引
-		charIndex[s[right]] = right
+		lastSeen[char] = right
 
-		// 注意窗口长度需要 +1
-		maxLen = max(maxLen, right-left+1)
+		maxLen = max(maxLen, right-left+1) // 差值计算的是区间，注意计数需 +1
 	}
 
 	return maxLen
